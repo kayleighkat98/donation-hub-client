6db31da4b3259be5cd70a2c56faec24a0a741b2b
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-10-28 18:34:17 +0000

    Update SitePage.js

--- src/routes/SitePage/SitePage.js
+++ src/routes/SitePage/SitePage.js
@@ -3,43 +3,14 @@ import './SitePage.css';
 import { Link } from 'react-router-dom'
 import SiteContext from '../../contexts/SiteContext';
 import image from '../../images/static-hub.jpg'
-// import InventroyService from '../../services/inventory-api-service';
-// import config from '../../config';
-
 
 class SitePage extends Component {
-    static contextType = HubContext;
-    
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         items: [],
-    //         isChecked: false
-    //     }
-    // }
+    static contextType = SiteContext;
+  
     state = {donated: false}
-    firstInput = React.createRef()
 
-    componentDidMount() {
-      this.firstInput.current.focus()
-    }
 
-    // handleCheckedItem = (event) => {
-    //     event.preventDefault();
-    //     fetch(`${config.API_Endpoint}/:site_id/items/:item_id`, {
-    //         method: 'GET',
-    //         header: {
-    //             'content-type': 'application/json'
-    //         }
-    //     })
-    //     .then(() => this.fetchData());
-    //     this.isChecked(event.target.value);
-    //     alert('Item added to your list');
-    //     this.setState({
-    //         isChecked: true
-    //     })
-    // }
-
+   
     handleSubmit = (event) => {
         event.preventDefault()
         alert('Thanks for your donation!')
@@ -52,10 +23,8 @@ class SitePage extends Component {
 
 
     render() {
-        // let item= this.state.items ? this.state.items : [];
-        // let handleSubmit= this.state.donated;
-        // console.log(`this.isChecked`)
-        const {donated} = this.state.handleSubmit
+       
+        const donated = this.state.handleSubmit
         return(
             <div className='site-page-container'>
                <h3>Drop Spot</h3>
@@ -63,15 +32,16 @@ class SitePage extends Component {
                <p>last updated: 10-07-2020</p>
                <h3>Items Needed:</h3>
                <ul>
-                   <li onSubmit={handleSubmit}>
-                        {/* <span className="donationItem" {...item.checked ? "donationItem-checked" : ''}>{item}</span>
-                            <button className="checkedToggle">
-                                <span className="checkbox">Donate Item</span>
-                            </button> */}
+                   <li>
+                      
+                            <button  onSubmit={this.handleSubmit}>
+                                <span >Donate Item</span>
+                            </button> 
                         <span>clothes || </span>
                         <span>quantity needed : 20  || </span>
                         <label htmlFor="quantity">amount donating:</label>
                         <input className="siteInput" type="number" id="quantity" name="quantity" min="0" max="100"/> 
+                        {console.log("submit handled")}
                    </li>
                </ul>
                <Link className="user-back-to-dash" to='/dashboard'>Back to Dashboard</Link>
