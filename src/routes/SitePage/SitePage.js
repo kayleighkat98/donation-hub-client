cfdb8579bc4d9152e91b5bf8c3aeff3fb3e06bf8
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-10-28 19:15:55 +0000

    Update SitePage.js

--- src/routes/SitePage/SitePage.js
+++ src/routes/SitePage/SitePage.js
@@ -7,14 +7,20 @@ import image from '../../images/static-hub.jpg'
 class SitePage extends Component {
     static contextType = SiteContext;
   
-    state = {donated: false}
-
+    state = {
+        donated: false,
+        donation: ''
+    }
 
    
     handleSubmit = (event) => {
         event.preventDefault()
+        this.setState({donation: ''})
         alert('Thanks for your donation!')
-    
+    } 
+
+    handleChange = (e) => {
+        this.setState({donation: e.target.value})
     } 
 
     static contextType = SiteContext;
@@ -26,8 +32,7 @@ class SitePage extends Component {
         return(
             <div className='site-page-container'>
                <h3>Drop Spot</h3>
-               <img alt='static-hub'src={image}/>
-               {/* <p>last updated: 10-07-2020</p> */}
+               <img alt='static-hub'src={image}/>            
                <h3>Items Needed:</h3>
                <form  onSubmit={this.handleSubmit}>
                 <ul>
@@ -36,10 +41,12 @@ class SitePage extends Component {
                                 <button type="submit" >
                                     <span >Donate Item</span>
                                 </button> 
-                            <span>clothes || </span>
-                            {/* <span>quantity needed : 20  || </span> */}
+                            <span>clothes || </span>                       
                             <label htmlFor="quantity">amount donating:</label>
-                            <input className="siteInput" type="number" id="quantity" name="quantity" min="0" max="100"/> 
+                            <input className="siteInput" type="number" id="quantity" name="quantity" min="0" max="100"
+                                value={this.state.donation}
+                                onChange={this.handleChange}
+                            /> 
                             
                     </li>
                 </ul>
