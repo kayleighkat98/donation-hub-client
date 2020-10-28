548f56ab9ff267a2faf4fa787121f028d7a9f9f1
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-10-28 18:47:21 +0000

    Update SitePage.js

--- src/routes/SitePage/SitePage.js
+++ src/routes/SitePage/SitePage.js
@@ -11,7 +11,7 @@ class SitePage extends Component {
 
 
    
-    handleClick = (event) => {
+    handleSubmit = (event) => {
         event.preventDefault()
         alert('Thanks for your donation!')
     
@@ -27,21 +27,23 @@ class SitePage extends Component {
             <div className='site-page-container'>
                <h3>Drop Spot</h3>
                <img alt='static-hub'src={image}/>
-               <p>last updated: 10-07-2020</p>
+               {/* <p>last updated: 10-07-2020</p> */}
                <h3>Items Needed:</h3>
-               <ul>
-                   <li>
-                      
-                            <button  onClick={this.handleClick}>
-                                <span >Donate Item</span>
-                            </button> 
-                        <span>clothes || </span>
-                        <span>quantity needed : 20  || </span>
-                        <label htmlFor="quantity">amount donating:</label>
-                        <input className="siteInput" type="number" id="quantity" name="quantity" min="0" max="100"/> 
-                        {console.log("submit handled")}
-                   </li>
-               </ul>
+               <form  onSubmit={this.handleSubmit}>
+                <ul>
+                    <li>
+                        
+                                <button type="submit" >
+                                    <span >Donate Item</span>
+                                </button> 
+                            <span>clothes || </span>
+                            {/* <span>quantity needed : 20  || </span> */}
+                            <label htmlFor="quantity">amount donating:</label>
+                            <input className="siteInput" type="number" id="quantity" name="quantity" min="0" max="100"/> 
+                            
+                    </li>
+                </ul>
+               </form>
                <Link className="user-back-to-dash" to='/dashboard'>Back to Dashboard</Link>
             </div>
         );
