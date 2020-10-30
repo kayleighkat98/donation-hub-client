3aeecd2dde44901d42edb1d9179bd0a57a4815ce
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-30 13:53:57 -0500

    insight on site fetch

--- src/services/site-service.js
+++ src/services/site-service.js
@@ -2,23 +2,27 @@ import config from '../config'
 import TokenService from './token-service'
 
 const SiteService = {
-    postLocation({label, address, description, lat, lon}) {
+    postLocation({ lat, lon,label, address, description, place_id}) {
         return fetch (`${config.API_ENDPOINT}/sites`,{
             method: 'POST',
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`,
+                'content-type': 'application/json'
             },
-            body: JSON.stringify({ label, address, description, lat, lon }),
+            body: JSON.stringify({ lat, lon,label, address, description, place_id }),
+            
         }).then(res =>
             (!res.ok)
               ? res.json().then(err => Promise.reject(err))
               : res.json()
         )
     },
+    // postItem({item_name, critical_amount})
     search(...box) {
         return fetch (`${config.API_ENDPOINT}/sites?rect=${box.join(',')}`,{
             method: 'GET',
         }).then(res =>
+            console.log(res.json)
             (!res.ok)
               ? res.json().then(err => Promise.reject(err))
               : res.json()
