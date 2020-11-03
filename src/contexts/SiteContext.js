1855105e97e5282674bf9bf87b3629cc1efcf336
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-03 12:22:49 -0600

    update site context for inventory

--- src/contexts/SiteContext.js
+++ src/contexts/SiteContext.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 const SiteContext = React.createContext({
   sites: {},
+  inventory: {},
+  setSites: ()=>{},
+  setInventory: () => {}
 });
 
 export default SiteContext
@@ -12,12 +15,24 @@ export class SiteProvider extends Component {
     super(props)
     this.state = {
       sites: [],
+      inventory: [],
+      setSites: () => {},
+      setInventory: () => {}
     };
   }
-
+  setSites = (sites) => {
+    this.setState({sites})
+  }
+  setInventory = (inventory) => {
+    this.setState({inventory})
+    console.log(this.state.inventory)
+  }
   render() {
     const value = {
       sites: this.state.sites,
+      inventory: this.state.inventory,
+      setInventory: this.setInventory,
+      setSites: this.setSites,
     }
     return(
       <SiteContext.Provider value={value}>
