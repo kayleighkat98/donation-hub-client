740706becb3ae56f6028b669e911a684cc8e47cc
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-03 17:28:04 -0600

    post item 201!!

--- src/services/site-service.js
+++ src/services/site-service.js
@@ -2,22 +2,36 @@ import config from '../config'
 import TokenService from './token-service'
 
 const SiteService = {
-    postLocation({ lat, lon, label, address, description, formatted_phone_number, place_id, url, website}) {
-        return fetch (`${config.API_ENDPOINT}/sites`,{
+    postLocation( {lat, lon, label, address, description, formatted_phone_number, place_id, url, website,items}) {
+           return fetch (`${config.API_ENDPOINT}/sites`,{
             method: 'POST',
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`,
                 'content-type': 'application/json'
             },
             body: JSON.stringify({lat, lon, label, address, description, formatted_phone_number, place_id, url, website}),
-
-        }).then(res =>
-            (!res.ok)
-              ? res.json().then(err => Promise.reject(err))
-              : res.json()
-        )
+            
+        }).then(response =>
+            (!response.ok)
+              ? response.json().then(err => Promise.reject(err))
+              : response.json()
+        ).then(data =>{
+            const site_id = data.id
+            items.map((item)=>{
+                let {item_name,critical_amount} = item
+                critical_amount = parseInt(critical_amount)
+                return fetch (`${config.API_ENDPOINT}/items/${site_id}/items`,{
+                    method: 'POST',
+                    headers: {
+                        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({item_name,critical_amount,site_id}),
+        
+                })
+            })
+        })
     },
-    // postItem({item_name, critical_amount})
     search(...box) {
         return fetch (`${config.API_ENDPOINT}/sites?rect=${box.join(',')}`,{
             method: 'GET',
