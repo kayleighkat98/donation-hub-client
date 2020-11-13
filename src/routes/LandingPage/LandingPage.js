4786bda5c8f90cd9267045aaa462a04bccfeb949
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-13 16:47:17 -0600

    update instructions

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -16,22 +16,23 @@ class LandingPage extends Component {
             <>
             <div className="landingText">
               <p>
-              <b>Stop</b> donating <b>whatever</b> to <b>wherever</b> or <b>whoever</b>, and <b>start</b> giving your belongings the second life they deserve by donating through <b>Donation Hub</b>! 
-                Welcome to Donation Hub, where you can find places near you and see what items they need. 
-                <b>You can</b> search for organizations near you without logging in, <b>or</b> make an account to give back to the community even more by letting us know when there are new donation centers in your area. 
+                Stop donating whatever to wherever or whoever, and start giving your belongings the second life they deserve
+                by donating through Donation Hub! Welcome to Donation Hub, where you can find the donation centers nearest to you and 
+                see what items they are most in need of. You can search for organizations near you without logging in, or make an account to give
+                back to the community even more through sharing your donations, flagging inaccurate information, and letting us know when there are
+                new donation centers in your area. 
               </p>
-              <p><b>Get started today!</b></p>
+              <p><b>Guest instructions:</b></p>
+              <p>For an existing location: search Dallas, TX</p>
+              <p>To demo adding a location: Login to the guest-dashboard with the credentials below and click "add a location" </p>
               <p>
-                If you are just a guest on the site, feel free to use the login credentials below.
-              </p>
-              <p>
-              <b>Username:</b> guest <b>Password:</b> Password!1
-              </p>
+                Username: guest Password: Password!1
+              </p> 
             </div>
              
             <SearchForm/>
 
-           </>
+           </> 
         );
     }
 }
