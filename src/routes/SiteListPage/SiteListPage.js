e72d432d93e220aafcb23ce7eaad7cf1c55ab20b
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-13 15:49:19 -0600

    part 1 recursive alert if no sites found

--- src/routes/SiteListPage/SiteListPage.js
+++ src/routes/SiteListPage/SiteListPage.js
@@ -15,6 +15,7 @@ class SiteListPage extends Component {
     },
   }
   state = { 
+    added: false,
     searchReturnedResults: null
   }
 
@@ -23,32 +24,43 @@ class SiteListPage extends Component {
   componentDidMount() {
     this.postRender();
   }
-
   componentDidUpdate() {
     this.postRender();
   }
-
+  recursiveContextAdd = (map, sites)=> {
+    if(this.state.added === true){
+      return
+    }else{
+      this.context.setSites({sites});
+      if (sites === 'There is no site'){
+        console.log('b')
+        this.setState({searchReturnedResults: false})
+      }
+      this.setState({added: true})
+      this.recursiveContextAdd(map, sites);
+    }
+  }
   setMapBoundsFromQuery(map, query) {
     if((typeof(query) !== 'string'))
-      return;
+    return;
     const args = query.substring(1).split(',');
     if(args.length < 4)
       return;
     // height of the top area, plus 4rem for App (margin-top + padding-top + padding-bottom + margin-bottom)
     const offset = this.uiRef.current.getBottom()
       + 4 * parseFloat(window.getComputedStyle(document.documentElement).fontSize);
-    const sw = { lat: +args[3], lng: +args[0] };
+      const sw = { lat: +args[3], lng: +args[0] };
     const ne = { lat: +args[1], lng: +args[2] };
     const bounds = new window.google.maps.LatLngBounds(sw, ne);
     map.fitBounds(bounds, { top: offset });
   }
   
   handleRender() {
-    if (this.state.searchReturnedResults === null){
+    if (this.context.sites===null){
       return(
         <>
           <p>
-            Content Loading...
+            Uh oh, an erro occured. Please refresh and search again.
           </p>
         </>
       )
@@ -66,7 +78,7 @@ class SiteListPage extends Component {
   async postRender() {
     const { map } = window;
     if(!map) // prevent this component from blowing up if tested without an attached map
-      return;
+    return;
 
     window.markers.forEach(marker => marker.setMap(null));
     window.markers = [];
@@ -74,37 +86,25 @@ class SiteListPage extends Component {
     this.setMapBoundsFromQuery(map, this.props.location.search);
     const bounds = map.getBounds();
     if(!bounds) // bail if the map doesn't have bounds
-      return;
-
+    return;
     const sw = bounds.getSouthWest(), ne = bounds.getNorthEast();
     const box = [ sw.lng(), ne.lat(), ne.lng(), sw.lat() ];
-    
     try {
       const sites = await SiteService.search(...box);
-      this.context.sites = sites;
-      return (
+      this.recursiveContextAdd(map,sites);
+      if(Array.isArray(sites)){
         sites.forEach(site => {
-        const marker = new window.google.maps.Marker({
-          label: site.label[0],
-          map: map,
-          position: { lat: site.lat, lng: site.lon },
-          animation: window.google.maps.Animation.DROP,
+          const marker = new window.google.maps.Marker({
+            label: site.label[0],
+            map: map,
+            position: { lat: site.lat, lng: site.lon },
+            animation: window.google.maps.Animation.DROP,
+          });
+          window.markers.push(marker);
+          marker.addListener('click', () => this.props.history.push(`/sites/${site.id}`));
         });
-        window.markers.push(marker);
-        marker.addListener('click', () => this.props.history.push(`/sites/${site.id}`));
-
-      })).then(()=>{
-        
-        if (window.markers.length > 1 ){
-          this.setState({searchReturnedResults: true})
-        }else{
-          this.setState({searchReturnedResults: false})
-        }
-        this.handleRender()
-      }).then(()=>{
-        console.log('test')
-      });
-    } catch (err) {
+      }
+      } catch (err) {
       console.log(err);
     }
   }
